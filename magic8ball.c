d64abee3661611e7b0785e5f6682f9266ff43c1c
Amin Mahmood <mam@naftech.com>
2024-02-01 01:52:45 -0600

    initial version

---
+++ magic8ball.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int result = 1;
+    printf("Welcome to the Random Dice Roll Simulator:\n");
+    printf("You rolled a: %d\n", result);
+    return 0;
+}
